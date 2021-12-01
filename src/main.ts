a6d2893a7b6ae697162c5c1dd04c4afc150a0a85
floriansmeyers <floriansmeyers@gmail.com>
2021-12-01 15:13:19 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Todo App')
+  .setDescription('API Description')
+  .setVersion('1.0')
+  .addTag('todos')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
