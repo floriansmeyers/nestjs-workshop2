e9fda8d9726081c1190650e5eabe6859dbc5c54d
floriansmeyers <floriansmeyers@gmail.com>
2022-03-11 12:12:29 +0100

    Cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   /*const app = await NestFactory.create(AppModule);*/
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({  // wrong!  in my case, anyway
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
   .setTitle('Todo App')
